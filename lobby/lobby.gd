c7497e5bac31f2f054f82917894df7327e42b7d4
Nolkaloid <noe.le.cam.nlc@gmail.com>
2023-05-27 21:11:45 +0200

    cleanup lobby

--- lobby/lobby.gd
+++ lobby/lobby.gd
@@ -1,11 +1,19 @@
-extends Node
+extends Control
+
+var players: Array = []
+
+func _ready():
+	players = $Players.get_children()
+	for p in players:
+		p.connect("type_selected", self, "_on_player_selection")
+		p.connect("type_deselected", self, "_on_player_deselection")
 
 
-var icons = []
+func _on_player_selection(type):
+	for p in players:
+		(p as PlayerIcon).set_type_available(type, false)
 
 
-
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_player_deselection(type):
+	for p in players:
+		(p as PlayerIcon).set_type_available(type, true)
